6221adf5e5f56314d1ff5d30ad8d540e32035e3a
UmamaheshMaxwell <maheshmeka16@gmail.com>
2022-02-15 18:02:13 +0530

    added react basics

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Welcome from "./1.components/class/Welcome";
+import { WelcomeOne } from "./1.components/class/WelcomeOne";
+import Greet from "./1.components/function/Greet";
+import { Named } from "./1.components/function/Named";
+import Hello from "./2.jsx/Hello";
+import Greeting from "./3.props/Greeting";
+import GreetingOne from "./3.props/GreetingOne";
+import GreetingTwo from "./3.props/GreetingTwo";
 
 function App() {
+  function sample(){
+    return "Hello"
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Greeting title="Mr.Scott" skill="Great Boss" />
+      <Greeting title="Mr.Adam" skill="Business Analyst"/>
+      <Greeting title="Mr.Tuan" skill="Tech Boss"/>
+      <GreetingOne title="Mr.Uma" skill="Javascript Developer" />
+      <GreetingTwo title="Mr.Scott" 
+                    skill="Great Boss" 
+                    sample={sample} 
+                    count={2}
+                    isAdmin={true}
+                    colors = {["red", "blue", "green"]}
+      />
+      {/* <Greet />
+      <Named />
+      <Welcome />
+      <WelcomeOne />
+      <Hello /> */}
     </div>
   );
 }
